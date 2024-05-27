264d451a63515ba58459e5d2a7fd44bce7fc4c30
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-05-27 15:43:16 -0300

    feat: create routes public and private controller

---
+++ src/Router/AuthGuard.tsx
@@ -0,0 +1,19 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+interface IAuthGuardProps {
+  isPrivate: boolean;
+}
+
+export function AuthGuard({ isPrivate }: IAuthGuardProps) {
+  const signedIn = false;
+
+  if (!signedIn && isPrivate) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (signedIn && !isPrivate) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+}
