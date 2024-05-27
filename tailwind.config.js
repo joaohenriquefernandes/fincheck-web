0bddb8bf576c7476e0777e26cf5408a5ded29967
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-05-27 15:06:44 -0300

    chore: configure eslint and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -175,9 +172,8 @@ export default {
           800: '#E8590C',
           900: '#D9480F',
         },
-      }
+      },
     },
   },
   plugins: [],
-}
-
+};
