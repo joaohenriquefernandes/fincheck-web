212ff4519f960b17e19779d1b9f57b1d98b5be94
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-05-28 08:45:45 -0300

    fix: remove flag type missing

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,15 +1,12 @@
 import { ComponentProps } from 'react';
 
-interface IButtonProps extends ComponentProps<'button'> {
-  type: string;
-}
+interface IButtonProps extends ComponentProps<'button'> {}
 
-export function Button({ type, ...props }: IButtonProps) {
+export function Button({ ...props }: IButtonProps) {
   return (
     <button
-      type={type}
-      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl text-white font-medium transition-all"
       {...props}
+      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 disabled:cursor-not-allowed disabled:text-gray-400 px-6 h-12 rounded-2xl text-white font-medium transition-all"
     />
   );
 }
