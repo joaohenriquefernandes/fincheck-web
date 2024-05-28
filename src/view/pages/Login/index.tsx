a9e4a6dc0cf5ddf102ddca6cd50023b62f666a63
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-05-28 09:28:24 -0300

    feat: add text style in header

--- src/view/pages/Login/index.tsx
+++ src/view/pages/Login/index.tsx
@@ -6,7 +6,7 @@ import { Input } from '../../components/Input';
 export function Login() {
   return (
     <>
-      <header className="flex flex-col items-center gap-4">
+      <header className="flex flex-col items-center gap-4 text-center">
         <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
           Entre em sua conta
         </h1>
