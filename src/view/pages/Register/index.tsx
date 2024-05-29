985c34f1d1badabae4be23ccfaf9cbfb3f22d827
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-05-29 09:49:49 -0300

    feat: create use register controller

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 
+import { useRegisterController } from './useRegisterController';
+
 export function Register() {
+  const { errors, handleSubmit, register } = useRegisterController();
   return (
     <>
       <header className="flex flex-col items-center gap-4 text-center">
@@ -25,10 +28,25 @@ export function Register() {
         </p>
       </header>
 
-      <form className="mt-[60px] flex flex-col gap-4">
-        <Input type="text" placeholder="Nome" name="name" />
-        <Input type="email" placeholder="E-mail" name="email" />
-        <Input type="password" placeholder="Senha" name="password" />
+      <form onSubmit={handleSubmit} className="mt-[60px] flex flex-col gap-4">
+        <Input
+          type="text"
+          placeholder="Nome"
+          error={errors.name?.message}
+          {...register('name')}
+        />
+        <Input
+          type="email"
+          placeholder="E-mail"
+          error={errors.email?.message}
+          {...register('email')}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          error={errors.password?.message}
+          {...register('password')}
+        />
 
         <Button type="submit">Criar Conta</Button>
       </form>
